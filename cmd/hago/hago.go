2fd75911ca9847745248225e50d357f23db444b4
Tim Meredith <tim@timmeredith.com>
2022-08-22 23:33:12 -0300

    Make resilient to missing network connection

--- cmd/hago/hago.go
+++ cmd/hago/hago.go
@@ -6,6 +6,7 @@ import (
   "gopkg.in/yaml.v2"
   "github.com/tradejmark/hago/pkg/hago"
   "fmt"
+  "time"
 )
 
 func main() {
@@ -14,17 +15,20 @@ func main() {
   configFile := os.Args[1]
   configData, err := ioutil.ReadFile(configFile)
   hago.Check(err)
-  
+
   err = yaml.Unmarshal(configData, &config)
   hago.Check(err)
 
   fmt.Println(config)
 
   broker := hago.GetBroker(config.Client.Name, config.Broker.Url, config.Broker.Port)
-  token := broker.Connect()
-  token.Wait()
-  if token.Error() != nil {
-    panic(token.Error())
+  for {
+    token := broker.Connect()
+    token.Wait()
+    if token.Error() == nil {
+      break
+    }
+    time.Sleep(5 * time.Second)
   }
   fmt.Println("connected")
   for _, dev := range config.Devices {
